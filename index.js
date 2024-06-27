d454472833790103d3e164678917aa3b8e3168ba
kate Emoefe <eseemoefe@gmail.com>
2024-06-27 12:02:01 +0100

    project

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http')
+
+http.createServer((req,res)=>{
+res.writeHead(200,{'content-Text': 'text/html'})
+res.write('<h1>Hello world</h1>')
+res.end()
+}).listen(3000)
+
+// send the response body as "Hello world"
+
+console.log('Server running at http://localhost:3000/');
